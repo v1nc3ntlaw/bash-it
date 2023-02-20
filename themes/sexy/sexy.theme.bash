f398499a0cadb764c692569daa69da1357f2c61e
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2023-02-21 04:49:58 +0800

    changed sexy theme style for personal

--- themes/sexy/sexy.theme.bash
+++ themes/sexy/sexy.theme.bash
@@ -22,6 +22,7 @@ if tput setaf 1 &> /dev/null; then
 		GREEN=$(tput setaf 2)
 		PURPLE=$(tput setaf 1)
 		WHITE=$(tput setaf 7)
+      CYAN=$(tput setaf 6)
 	fi
 	BOLD=$(tput bold)
 	RESET=$(tput sgr0)
@@ -31,22 +32,27 @@ else
 	GREEN="\033[1;32m"
 	PURPLE="\033[1;35m"
 	WHITE="\033[1;37m"
+    CYAN="\033[1;36m"
 	BOLD=""
 	RESET="\033[m"
 fi
 
 parse_git_dirty() {
-	[[ $(git status 2> /dev/null | tail -n1 | cut -c 1-17) != "nothing to commit" ]] && echo "*"
+	if [[ $(git status 2> /dev/null | tail -n1 | cut -c 1-17) != "nothing to commit" ]];then
+    echo "✗"
+  else
+    echo "✓"
+  fi
 }
 parse_git_branch() {
-	git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+	git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/$SCM_GIT_CHAR \1 $(parse_git_dirty)/"
 }
 env_prompt() {
 	echo -e "($(virtualenv_prompt)$(condaenv_prompt))"
 }
 
 function prompt_command() {
-	PS1="\[${BOLD}${MAGENTA}\]\u \[$WHITE\]at \[$ORANGE\]\h \[$WHITE\]in \[$GREEN\]\w\[$WHITE\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \" on \")\[$PURPLE\]\$(parse_git_branch)\[$WHITE\]\n\$ \[$RESET\]"
+	PS1="\[${BOLD}${MAGENTA}\]\u \[$RESET\]at \[${BOLD}$ORANGE\]\h \[$RESET\]in \[${BOLD}$GREEN\]\w \[$RESET\]\nwith \[${BOLD}$PURPLE\]\$(ruby_version_prompt)\[$RESET\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \" on \")\[${BOLD}$CYAN\]\$(parse_git_branch) \[$RESET\]\$ "
 
 	if [ "$SEXY_THEME_SHOW_PYTHON" = true ]; then
 		PS1="\[${BOLD}${WHITE}\]$(env_prompt) "$PS1
